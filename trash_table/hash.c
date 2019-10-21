1523cfb87a9982831181a3d0f279e701909b6dd4
Patrick DeYoreo <29614644+patrickdeyoreo@users.noreply.github.com>
2019-10-21 15:12:14 -0700

    Moved djb2 into hash.c

--- trash_table/hash.c
+++ trash_table/hash.c
@@ -1,7 +1,24 @@
 #include "trash_table.h"
 
 /**
- * hash - get the index mapped to by a key
+ * djb2 - compute a hash using the djb2 algorithm
+ * @str: a string to hash
+ *
+ * Return: the hash of str
+ */
+size_t djb2(const unsigned char *str)
+{
+	size_t hash = 5381;
+	unsigned int c = 0;
+
+	while ((c = *str++))
+		hash += (hash << 5) + c;
+
+	return (hash);
+}
+
+/**
+ * hash - get the index mapped to by a given key
  * @key: the key to map to an array index
  * @size: the array size
  *
