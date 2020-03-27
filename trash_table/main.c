ed688384dc68102057e4035cce204d09eaa6a959
Patrick DeYoreo <29614644+patrickdeyoreo@users.noreply.github.com>
2020-03-27 13:30:29 -0700

    Remove calls to naive level-order code

--- trash_table/main.c
+++ trash_table/main.c
@@ -6,28 +6,6 @@
 #define SEPARATOR "---"
 
 /**
- * basename - get the part of a string following the last '/'
- * @str: a pointer to the string to search
- *
- * Return: If '/' was not found, return str.
- * Otherwise, return a pointer to the character following the final '/'.
- */
-char *basename(char *str)
-{
-	char *pos = str;
-
-	if (str)
-	{
-		while (*str)
-		{
-			if (*str++ == '/')
-				pos = str;
-		}
-	}
-	return (pos);
-}
-
-/**
  * main - example usage
  * @argc: the argument count
  * @argv: the argument vector
@@ -41,17 +19,17 @@ int main(int argc, char **argv)
 
 	if (argc == 1)
 	{
-		fprintf(stderr, "usage: %s key=value ...\n", basename(*argv));
+		fprintf(stderr, "Usage: %s key=value ...\n", *argv);
 		return (2);
 	}
-	puts("Creating dictionary...");
+	puts("Creating table...");
 	tt = trash_table_new(TABLESIZE);
 	if (!tt)
 	{
-		fprintf(stderr, "%s: Not enough memory.\n", basename(*argv));
+		fprintf(stderr, "Failed to allocate %dB\n", sizeof(*tt->array) * TABLESIZE);
 		return (1);
 	}
-	puts("Populating dictionary...");
+	puts("Populating...");
 	while ((key = *(++argv)))
 	{
 		value = strchr(key, '=');
@@ -60,17 +38,16 @@ int main(int argc, char **argv)
 		trash_table_set(tt, key, value);
 	}
 	puts(SEPARATOR);
-	trash_table_print(tt);
-	puts(SEPARATOR);
 	printf("Size  : %lu\n", trash_tree_size(tt->root));
 	printf("Height: %lu\n", trash_tree_height(tt->root));
 	printf("Leaves: %lu\n", trash_tree_leaves(tt->root));
+	puts("Checking tree...");
 	if (trash_tree_black_balanced(tt->root))
-		puts("Tree is balanced.");
+		puts("The tree is balanced.");
 	else
-		puts("Tree is not balanced!");
+		puts("Oh no! The tree is not balanced!");
 	puts(SEPARATOR);
-	puts("Deleting dictionary...");
+	puts("Deleting table...");
 	trash_table_delete(&tt);
 	return (0);
 }
