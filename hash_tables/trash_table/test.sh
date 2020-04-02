1313df49474cbf64c82c8b10510d8ce1014b2efc
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-04-02 10:29:06 -0700

    Resolved bash core dump

--- hash_tables/trash_table/test.sh
+++ hash_tables/trash_table/test.sh
@@ -5,8 +5,9 @@
 # all source files in the directory heierarchy rooted at the location
 # of this file will be passed to gcc for compilation. If called with
 # arguments, they and they alone will be passed to gcc.
-# usage: test.sh [SOURCE] ...
-
+#
+# Usage: test.sh [SOURCE] ...
+######################################################################
 
 # Requests a response from the user regarding a "yes-or-no" question
 # Usage: confirm [PROMPT]
@@ -18,40 +19,49 @@ confirm_exit()
   local -
   set -o errexit
   local -u REPLY
+  echo
   while read -r -N 1 -p 'Are you sure you want to exit? [Y/n] '
   do
-    echo
-    case ${REPLY} in
+    case ${REPLY/$'\n'} in
       Y) return 0 ;;
       N) return 1 ;;
+      ?) echo     ;;
     esac
   done
 }
 
-
 # Configure the environment and compile the executable. If this fails, exit.
 set -o errexit
 
-# If the shell is interactive, request confirmation before exiting upon
-# receiveing Ctrl-C (SIGINT). Otherwise exit quieltly with the proper status.
-trap 'echo
-[[ -t 0 ]] && confirm_exit || {
-  trap INT && kill -s INT "$$" || exit 130
-}' INT
-
 # Initialize loop variables.
+unset ascii
+unset index
+unset input
 chars=( )
 items=( )
-printf -v beg '%d' "'a"
-printf -v end '%d' "'z"
+printf -v lower '%d' "'A"
+printf -v upper '%d' "'z"
 
-# Enter the directory in which this script resides
-CDPATH='' cd -- "${BASH_SOURCE[0]%/*}"
+# If the shell is interactive, request confirmation before exiting upon
+# receiveing Ctrl-C (SIGINT). Otherwise exit quieltly with the proper status.
+trap '
+echo
+if ! [[ -t 0 ]] || confirm_exit
+then
+  exec {input}>&-
+  trap INT && kill -INT "$$" || exit 130
+fi
+' INT
 
 # Make a directory for temporary files and set a trap to remove it upon exit.
 unset tmpdir
-trap 'rm -fr -- "${tmpdir}" 2> /dev/null' EXIT
+trap '
+rm -fr -- "${tmpdir}" 2> /dev/null
+' EXIT
 tmpdir="$(mktemp -d --tmpdir)"
+
+# Enter the directory in which this script resides
+CDPATH='' cd -- "${BASH_SOURCE[0]%/*}"
 
 # Try to compile all non-hidden .c files from all non-hidden directories at
 # or below the location of this script. Provide file or directory names as
@@ -62,35 +72,32 @@ then
   set -- ./**/*.c
   shopt -u globstar nullglob
 fi
-gcc -g -Wall -Werror -Wextra -pedantic -o "${tmpdir}/exe" "$@"
+gcc -g -Wall -Werror -Wextra -pedantic -o "${tmpdir}/main" "$@"
 
 # Environment configured. Compilation successful.
 set +o errexit
 
-
 # Execute program with randomly generated arguments.
-# shellcheck disable=SC2059
-for (( ord = beg; ord <= end; ++ord ))
+for (( ascii = lower; ascii <= upper; ascii = (16#${ascii}) + 1 ))
 do
-  printf -v 'hex' '\\x%02x' "${ord}"
+  printf -v 'ascii' '%02x' "${ascii}"
 
-  printf -v "chars[${ord}]" "${hex}"
+  printf -v "chars[$((16#${ascii}))]" '%b' "\x${ascii}"
 
-  for char in "${chars[@]}"
+  for index in "${!chars[@]}"
   do
-    items+=("${chars[-1]}${char}=${RANDOM}")
+    items+=("$((16#${ascii}+index))=${chars[16#${ascii}]}+${chars[index]}")
   done
-
-  if IFS=$'\n' read -a items -d '' -r
+  if wait "$!" && IFS=$'\n' read -a items  -d '' -r -u "$((input))"
   then
-    "${tmpdir}/exe" "${items[@]}"
-  fi < <(shuf -e -- "${items[@]}" && printf '\0')
+    "${tmpdir}/main" "${items[@]}"
+  fi {input}< <(shuf --echo -- "${items[@]}" && printf '\0')
 
-  if (( ord != end ))
+  if (( 16#${ascii} != upper ))
   then
-    read -r -t 0.8
+    sleep 1
     echo
     echo '****************'
     echo
-  fi <> <(:)
+  fi {input}>&-
 done
